006e2e8c24ac2fb2d5a5ffd04926272eea0a2712
hoangduyqsqt <duymapqsqt@gmail.com>
2024-11-29 15:26:22 +0700

    push

--- frontend/src/pages/home/Banner.jsx
+++ frontend/src/pages/home/Banner.jsx
@@ -7,7 +7,7 @@ const Banner = () => {
   return (
     <div className='section__container header__container'>
         <div className='header__content z-30'>
-            <h4 className='uppercase'>UP TO 20% Discount on</h4>
+            <h4 className='uppercase'>UP TO 10% Discount on</h4>
             <h1>Girl Fashion</h1>
             <p>Discover the latest trends and express your unique style with our Women Fashion website. Explore a curated collection of clothing, accessories, and footwear that caters to every taste and occasion.</p>
             <button className='btn'><Link to='/shop'>EXPLORE NOW</Link></button>
